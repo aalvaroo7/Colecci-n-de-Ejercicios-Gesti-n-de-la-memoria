22d742107641eac8133e46f50692eec0ca0c4749
aalvaroo7 <almarrom@myuax.com>
2023-12-14 00:12:34 +0100

    comit

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,4 +1,3 @@
-
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +8,7 @@
 #define SIZE 4096
 
 int main() {
-    // Crear una región de memoria compartida
+    // Crear una región de memoria compartida de SIZE bytes
     char *shared_memory = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
     // Verificar si la asignación de memoria compartida fue exitosa
@@ -17,25 +16,22 @@ int main() {
         perror("mmap");
         exit(EXIT_FAILURE);
     }
-    // Crear un nuevo proceso
+
+    // Crear un nuevo proceso usando fork()
     pid_t pid = fork();
 
-    // Verificar errores en la creación del proceso
+    // Verificar errores en la creación del proceso hijo
     if (pid < 0) {
         perror("fork");
-        // Liberar la memoria compartida antes de salir en caso de error
-        munmap(shared_memory, SIZE);
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
         // Código ejecutado por el proceso hijo
+
         // Imprimir el contenido de la memoria compartida
         printf("Child reads: %s\n", shared_memory);
 
         // Liberar la memoria compartida utilizada por el hijo
-        if (munmap(shared_memory, SIZE) == -1) {
-            perror("munmap");
-            exit(EXIT_FAILURE);
-        }
+        munmap(shared_memory, SIZE);
 
         // Salir con éxito
         exit(EXIT_SUCCESS);
@@ -49,10 +45,7 @@ int main() {
         wait(NULL);
 
         // Liberar la memoria compartida utilizada por el padre
-        if (munmap(shared_memory, SIZE) == -1) {
-            perror("munmap");
-            exit(EXIT_FAILURE);
-        }
+        munmap(shared_memory, SIZE);
     }
 
     // Terminar el programa con éxito
